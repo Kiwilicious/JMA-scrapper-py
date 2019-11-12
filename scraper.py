eb7516c69ad3288aaf2b98bcc1c5b1a6773548d2
James Hwang <jhwang137@gmail.com>
2019-11-12 10:50:26 -0600

    Clean up python file

--- scraper.py
+++ scraper.py
@@ -3,16 +3,16 @@ import urllib.request
 import urllib.parse
 import json
 
-baseURL = "http://www.data.jma.go.jp/obd/stats/etrn/select/prefecture00.php?prec_no=&block_no=&year=&month=&day=&view=a1"
-dataLinkText = "観測開始からの月ごとの値を表示"
+baseURL = 'http://www.data.jma.go.jp/obd/stats/etrn/select/prefecture00.php?prec_no=&block_no=&year=&month=&day=&view=a1'
+dataLinkText = '観測開始からの月ごとの値を表示'
 
 
 def getAllPrefs(URL):
     content = urllib.request.urlopen(URL)
-    soup = BeautifulSoup(content, features="html.parser")
+    soup = BeautifulSoup(content, features='html.parser')
     prefectures = {}
 
-    for pref in soup.findAll('area', attrs={"shape": "rect"}):
+    for pref in soup.findAll('area', attrs={'shape': 'rect'}):
         prefName = pref["alt"]
         prefLink = urllib.parse.urljoin(URL, pref["href"])
         prefectures[prefName] = prefLink
@@ -22,12 +22,12 @@ def getAllPrefs(URL):
 
 def getAllPrefCities(prefURL):
     content = urllib.request.urlopen(prefURL)
-    soup = BeautifulSoup(content, features="html.parser")
+    soup = BeautifulSoup(content, features='html.parser')
     cities = {}
 
-    for city in soup.findAll("area", attrs={"shape": "rect"}):
-        cityName = city["alt"]
-        cityLink = urllib.parse.urljoin(prefURL, city["href"])
+    for city in soup.findAll('area', attrs={'shape': 'rect'}):
+        cityName = city['alt']
+        cityLink = urllib.parse.urljoin(prefURL, city['href'])
         cities[cityName] = cityLink
 
     return cities
@@ -35,19 +35,19 @@ def getAllPrefCities(prefURL):
 
 def getTempDataLink(cityURL):
     content = urllib.request.urlopen(cityURL)
-    soup = BeautifulSoup(content, features="html.parser")
+    soup = BeautifulSoup(content, features='html.parser')
 
     for anchor in soup.findAll("a"):
         if anchor.text.strip() == dataLinkText:
-            return urllib.parse.urljoin(cityURL, anchor["href"])
+            return urllib.parse.urljoin(cityURL, anchor['href'])
 
 
 def getCityTemps(dataURL):
     content = urllib.request.urlopen(dataURL)
-    soup = BeautifulSoup(content, features="html.parser")
+    soup = BeautifulSoup(content, features='html.parser')
     tempsByYear = {}
 
-    for tr in soup.findAll('tr', attrs={"class": "mtx"}):
+    for tr in soup.findAll('tr', attrs={'class': 'mtx'}):
         year = tr.find('a').text.strip()
         tempsByMonth = []
 
@@ -56,8 +56,8 @@ def getCityTemps(dataURL):
             temp = td.text.strip()
             if temp != '':
                 temp = temp.split()[0]
-            if temp == "×":
-                temp = ""
+            if temp == '×':
+                temp = ''
             tempsByMonth.append(temp)
 
         tempsByYear[year] = tempsByMonth
@@ -66,7 +66,7 @@ def getCityTemps(dataURL):
 
 
 def outputDataAsJSON(data):
-    with open("data.json", 'w', encoding="utf-8") as json_file:
+    with open('data.json', 'w', encoding='utf-8') as json_file:
         json.dump(data, json_file, indent=4, ensure_ascii=False)
 
 
@@ -77,7 +77,7 @@ def main():
     for prefName, prefLink in prefectures.items():
         cityData = {}
         cities = getAllPrefCities(prefLink)
-        print(f"On {prefName}")
+        print(f'On {prefName}')
 
         for cityName, cityLink in cities.items():
             tempDataLink = getTempDataLink(cityLink)
